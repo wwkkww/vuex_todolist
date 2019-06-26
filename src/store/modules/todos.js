cbee9b71433a18494de6e74993dfa18036847780
kevinW <wongkokwah@gmail.com>
2019-06-26 18:10:01 +0800

    init commit

---
+++ src/store/modules/todos.js
@@ -0,0 +1,36 @@
+import axios from 'axios'
+
+const state = {
+  todos: [
+    {
+      id: 1,
+      title: 'Todo One'
+    }, {
+      id: 2,
+      title: 'Todo Two'
+    }
+  ]
+}
+
+const getters = {
+  // array name for mapGetters
+  allTodos: (state) => state.todos
+}
+
+const actions = {
+  async fetchTodos({ commit }) {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
+    commit('setTodos', response.data);
+  }
+}
+
+const mutations = {
+  setTodos: (state, todos) => (state.todos = todos)
+}
+
+export default({
+  state,
+  getters,
+  actions,
+  mutations
+})
